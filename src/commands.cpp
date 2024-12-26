5be46da7256c3feabaa6f776fe799a7c22c501bb
Hemang Choudhary <hemangc37@gmail.com>
2024-12-26 14:36:21 +0530

    stage-0

--- src/commands.cpp
+++ src/commands.cpp
@@ -35,10 +35,12 @@ void execute_echo(const string &input,const vector<string> &args)
                    if(result[i] == '\\' && k%2 != 0){
                      k++;
                     result.erase(i, 2);
+                   cout << result;
                     i++;
                    }
-                   else if(k%2 == 0 && result[i] ==  '\\'){
+                   else if(k%2 == 0 && result[i] ==  '\\' && k != 0){
                     result[i] = ' ';
+                    cout << result;
                     k = 0;
                    }
                 }
@@ -51,12 +53,14 @@ void execute_echo(const string &input,const vector<string> &args)
           if(backslash_count%2 == 0){
                 for(int i = 0; i < result.length();i++){
                    if(result[i] == '\\' && k%2 != 0){
-                     k++;
-                   result.erase(i, 2);
-                   i++;
+                    k++;
+                    result.erase(i, 2);
+                    cout << result;
+                    i++;
                    }
-                   else if(k%2 == 0 && result[i] ==  '\\'){
+                   else if(k%2 == 0 && result[i] ==  '\\' && k != 0){
                     result[i] = ' ';
+                    cout << result;
                     k = 0;
                    }
                 }
